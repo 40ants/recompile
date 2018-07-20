134af3e06da2b9d13f5367d2b710e80f6cb34668
Alexander Artemenko <svetlyak.40wt@gmail.com>
2018-07-20 10:05:21 +0300

    Removed previous version.

--- recompile.lisp
+++ recompile.lisp
@@ -26,29 +26,3 @@
                (asdf:load-system system
                                  :verbose verbose
                                  :force t))))
-
-
-;; (defun recompile (system-name &key (verbose t) (break-on-warnings t))
-;;   "Recompile given system. By default all warnings are considered as errors.
-
-;;    If system uses :package-inferred-system class, then all subsystems are also
-;;    recompiled."
-;;   (declare (ignorable break-on-warnings))
-;;   (let* ((all-systems (asdf:already-loaded-systems))
-;;          ;; #+ccl
-;;          ;; (ccl:*break-on-warnings* break-on-warnings)
-;;          ;; This does not work for SBCL's notes
-;;          ;; (asdf:*compile-file-warnings-behaviour* (if break-on-warnings
-;;          ;;                                             :error
-;;          ;;                                             :warn))
-;;          (systems-to-recompile
-;;            (list* ;; system-name
-;;                   (loop for system in all-systems
-;;                         for primary-name = (asdf:primary-system-name system)
-;;                         when (string-equal system-name
-;;                                            primary-name)
-;;                           collect system))))
-
-;;     (asdf:load-system system-name
-;;                       :verbose verbose
-;;                       :force systems-to-recompile)))
